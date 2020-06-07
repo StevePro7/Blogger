788022ef3449000ef66149f0acd115431dbcfb82
StevePro7 <steveprozune@hotmail.com>
2020-06-07 13:12:36 +0100

    no message

---
+++ SGDK_Setup/libres/libres.h
@@ -0,0 +1,9 @@
+#ifndef _E_SGDK_VSTUDIO_RES_LIBRES_H_
+#define _E_SGDK_VSTUDIO_RES_LIBRES_H_
+
+extern const Bitmap logo_lib;
+extern const TileSet font_lib;
+extern const Palette font_pal_lib;
+extern const u8 stop_xgm[172];
+
+#endif// _E_SGDK_VSTUDIO_RES_LIBRES_H_
