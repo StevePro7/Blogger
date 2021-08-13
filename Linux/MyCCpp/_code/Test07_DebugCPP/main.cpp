dbd01f934f1486521255415a875f2a9d06d1b61e
StevePro7 <steveprozune@hotmail.com>
2021-08-13 19:04:40 +0100

    no message

---
+++ Linux/MyCCpp/_code/Test07_DebugCPP/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 8;
+    cout << "Hello World C++..!" << endl;
+    cout << "Hello World C++..!" << endl;
+    cout << "Hello World C++..!" << endl;
+    cout << x << endl;
+    return 0;
+}
