41cb57bbf76c8a3e4a1efda48ee5a23e3fa7e545
StevePro7 <steveprozune@hotmail.com>
2021-08-16 14:40:52 +0100

    no message

---
+++ Linux/MyCCpp/_code/Test08_ModSec/main.c
@@ -0,0 +1,11 @@
+#include "modsecurity/modsecurity.h"
+#include <stdio.h>
+
+void main()
+{
+    printf("Hello world C\n");
+    printf("Hello world C\n");
+    printf("Hello world C\n");
+    printf("Hello world C\n");
+    printf("Hello world C\n");
+}
