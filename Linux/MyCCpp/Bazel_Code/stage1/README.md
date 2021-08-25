77dea8cdcb21b493e2f201956dc2573539835b09
StevePro7 <steveprozune@hotmail.com>
2021-08-25 10:05:01 +0100

    https://www.youtube.com/watch?v=-TUogVOs1Qg

---
+++ Linux/MyCCpp/Bazel_Code/stage1/README.md
@@ -0,0 +1,8 @@
+Build
+Ctrl + Shift + B
+
+Run
+F5
+
+
+"stopAtEntry": false
---
+++ Linux/MyCCpp/Bazel_Code/stage2/main/hello-world.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  int num = 10;
+  cout << "Hello C++!!" << endl;
+  cout << "The value of num = " << num << endl;
+  return 0;
+}
