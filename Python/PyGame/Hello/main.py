40e2885d369cf570d530fb537cff0459bc7a33ee
StevePro7 <steveprozune@hotmail.com>
2026-04-18 16:35:02 +0100

    HelloPygame
    HelloPygame

---
+++ Python/PyGame/Hello/main.py
@@ -0,0 +1,13 @@
+import pygame
+import sys
+
+pygame.init()
+screen = pygame.display.set_mode((640, 480))
+pygame.display.set_caption("Hello World")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
